efd9d9ba50620515c986fd18979de2d759698e8d
Shuai Zhao <shuaizhao92@gmail.com>
2018-01-02 02:23:15 -0500

    Create setup.sh

---
+++ spark/setup.sh
@@ -0,0 +1,6 @@
+ # BerkeleyX: CS100.1x Introduction to Big Data with Apache Spark
+ # 1. Install Vagrant and VirtualBox (Turn off secure boot in BIOS)
+ # 2. vagrant up
+ # 3. vagrant global-status (to check running vagrant vms)
+ # 4. vagrant halt (stop vms before turning off/logining out home os)
+ # 5. vagrant destroy (destory the vm)
