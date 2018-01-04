f63e0228782ec7b960dca743bc5441819db8e075
Shuai Zhao <shuaizhao92@gmail.com>
2018-01-04 12:36:43 -0500

    Update and rename search_2d_matrix.py to 028SearchA2dMatrix.py

--- lintcode/028SearchA2dMatrix.py
+++ lintcode/028SearchA2dMatrix.py
@@ -1,4 +1,4 @@
-
+'''
 
 Write an efficient algorithm that searches for a value in an m x n matrix, return the occurrence of it.
 
@@ -7,6 +7,7 @@ This matrix has the following properties:
     Integers in each row are sorted from left to right.
     Integers in each column are sorted from up to bottom.
     No duplicate integers in each row or column.
+'''
 
 
 
